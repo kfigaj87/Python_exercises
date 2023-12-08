688f9b3af97e4292c500a7ef568f23922cb2cea0
kfigaj <kfigaj87@gmail.com>
2023-12-08 10:40:40 +0100

    commit no1

---
+++ variables.py
@@ -0,0 +1,25 @@
+# Exercise 1
+
+# In this exercise, assign values of the appropriate types to the following variables.
+
+# For variable:
+
+#   var1 - assign any string
+#   var2 - assign any integer
+#   var3 - assign any float
+#   var4 - assign a boolean value
+
+def exercise():  # DO NOT CHANGE THIS, IT MUST BE AT THE TOP
+
+    var1 = "python"
+    var2 = 100
+    var3 = 1.25
+    var4 = True
+
+    # DO NOT CHANGE THIS, IT MUST BE AT THE BOTTOM
+    return (var1, var2, var3, var4)
+
+
+# Call the function and print the results
+result = exercise()
+print(result)
